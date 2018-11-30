58e358803de1d763499c67988be500b5a8283280
zhangxy <qq2003qq@126.com>
2018-11-30 18:13:09 +0800

    修改数据库

--- Bank_Transfer.py
+++ Bank_Transfer.py
@@ -7,8 +7,8 @@ class Money():
 			host="127.0.0.1",
 			port=3306,
 			user='root',
-			passwd='Aa12345',
-			db='sdk_log'
+			passwd='****',
+			db='test'
 			)
 		self.cursor = self.conn.cursor();
 		self.table = "money"
